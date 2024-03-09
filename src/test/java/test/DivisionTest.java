37d3866a5c15eb14aa1e7843c492b9dd891f878c
Camilo Prieto <juanc.prietom@konradlorenz.edu.co>
2024-03-09 17:23:47 -0500

    Entrega final Division
    Se realizó el código para que funcionara el programa y se agregaron mas casos de prueba para corroborar que el programa funcione correctamente.

--- src/test/java/test/DivisionTest.java
+++ src/test/java/test/DivisionTest.java
@@ -1,7 +1,6 @@
 package test;
 
 import ejercicios.Division;
-import ejercicios.SetDeTenis;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,12 +15,35 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DivisionTest {
     @Test
     public void testDivisionExacta() {
-        String valorEsperado = "La división es exacta. \n"
+        String valorEsperado = "La división no es exacta. \n"
                 + "Cociente: 2\n"
                 + "Residuo: 4";
         String valorActual = Division.evaluar(14, 5);
         assertEquals(valorEsperado, valorActual);
     }
     
-    // TODO: Agrega tus otros casos de prueba aquí
+    @Test
+    public void testDivisionNoExacta(){
+        String valorEsperado = "La división es exacta. \n"
+                + "Cociente: 10\n"
+                + "Residuo: 0";
+        String valorActual = Division.evaluar(100, 10);
+        assertEquals(valorEsperado, valorActual);
+    }
+    @Test
+    public void testDivisionExactaDos(){
+        String valorEsperado = "La división es exacta. \n"
+                + "Cociente: 24\n"
+                + "Residuo: 0";
+        String valorActual = Division.evaluar(48, 2);
+        assertEquals(valorEsperado, valorActual);
+    }
+    @Test
+    public void testDivisionNoExactaDos(){
+        String valorEsperado = "La división no es exacta. \n"
+                + "Cociente: 38\n"
+                + "Residuo: 1";
+        String valorActual = Division.evaluar(77, 2);
+        assertEquals(valorEsperado, valorActual);
+    }
 }
