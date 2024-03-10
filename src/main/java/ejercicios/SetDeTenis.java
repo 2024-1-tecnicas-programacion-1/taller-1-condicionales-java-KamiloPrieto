d8acdcef54c6885a7bd2a5faf2f5012e0e232d3b
Camilo Prieto <juanc.prietom@konradlorenz.edu.co>
2024-03-09 22:50:07 -0500

    Entrega final Set de tenis
    Se realizo el código para que cumpliera con lo solicitado en el problema, y se le agrego nuevos casos de prueba al test para verificar que el programa funcionara correctamente, lo cual si hace el código.

--- src/main/java/ejercicios/SetDeTenis.java
+++ src/main/java/ejercicios/SetDeTenis.java
@@ -7,19 +7,30 @@ import java.util.Scanner;
  * @author danielsanchez
  */
 public class SetDeTenis {
+
     public static String evaluar(int numVictoriasA, int numVictoriasB) {
-        // TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-        return "";
+        if (Math.abs(numVictoriasA - numVictoriasB) > 2 || Math.max(numVictoriasA, numVictoriasB)> 7) {
+            return "Inválido";
+        } else if (numVictoriasA >= 6 && numVictoriasA - numVictoriasB == 2) {
+            return "Ganó A";
+
+        } else if (numVictoriasB >= 6 && numVictoriasB - numVictoriasA == 2) {
+            return "Ganó B";
+        } else if (numVictoriasA <= 6 && numVictoriasB <= 6) {
+            return "Aún no termina";
+
+        } 
+        return "Inválido";
     }
-    
+
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-        
+
         System.out.print("Los juegos ganador por A:");
         int numVictoriasA = lector.nextInt();
         System.out.print("Los juegos ganador por B:");
         int numVictoriasB = lector.nextInt();
-        
+
         String respuesta = evaluar(numVictoriasA, numVictoriasB);
         System.out.println(respuesta);
     }
