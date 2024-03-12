b8fb93ae4ba7add9d292af940a368c38c4ff94ef
Camilo Prieto <juanc.prietom@konradlorenz.edu.co>
2024-03-11 22:58:13 -0500

    Correccion del bug Git Kreken
    Se realizo el arreglo del bug producido a la hora de subir la primera vez en Git Kraken que corrompía el código y lo subía doble.

--- src/main/java/ejercicios/AnnosBisiestos.java
+++ src/main/java/ejercicios/AnnosBisiestos.java
@@ -4,7 +4,7 @@ import static ejercicios.SetDeTenis.evaluar;
 import java.util.Scanner;
 
 public class AnnosBisiestos {
-<<<<<<< Updated upstream
+
     public static String evaluar(int anno){
         if(anno%4 == 0){
             return anno + " es bisiesto";
@@ -13,26 +13,8 @@ public class AnnosBisiestos {
         }else{
             return anno + " no es bisiesto";
         }
-=======
-
-    public static String evaluar(int anno) {
-        if (anno % 4 != 0) {
-            return anno + " no es bisiesto";
-        } else if (anno % 100 != 0) {
-
-            return anno + " es bisiesto";
-        } else if (anno % 400 == 0) {
-
-            return anno + " es bisiesto";
-        } else {
-            return anno + " no es bisiesto";
-
-        }
-
->>>>>>> Stashed changes
     }
-
-    public static void main(String[] args) {
+ public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
         System.out.print("Año:");
         int anno = lector.nextInt();
